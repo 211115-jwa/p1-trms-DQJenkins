754156eebf398221ea2f21dff099933286b5c1a0
Dana Jenkins <94149057+DQJenkins@users.noreply.github.com>
2022-01-02 11:01:41 -0800

    Setup ServiceTest classes

--- trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
+++ trms-back/src/test/java/com/revature/services/EmployeeServiceTest.java
@@ -1,5 +1,33 @@
 package com.revature.services;
 
-public class EmployeeServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.revature.beans.Comment;
+import com.revature.beans.Employee;
+import com.revature.beans.Reimbursement;
+
+
+public class EmployeeServiceTest {
+	
+	//Service methods to test
+	//public Map<String, Set<Object>> getRequestOptions();
+	//public int submitReimbursementRequest(Reimbursement request);
+	//public Set<Reimbursement> getReimbursementRequests(Employee requestor);
+	//public Set<Comment> getComments(Reimbursement request);
+	//public int addComment(Comment comment);
+	//public Employee getEmployeeById(int empId);
 }
--- trms-back/src/test/java/com/revature/services/RequestReviewServiceTest.java
+++ trms-back/src/test/java/com/revature/services/RequestReviewServiceTest.java
@@ -1,5 +1,30 @@
 package com.revature.services;
 
-public class RequestReviewServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.revature.beans.Comment;
+import com.revature.beans.Employee;
+import com.revature.beans.Reimbursement;
+
+
+public class RequestReviewServiceTest {
+	
+	//Service methods to test
+	//public Set<Reimbursement> getPendingReimbursements(Employee approver);
+	//public void approveRequest(Reimbursement request);
+	//public void rejectRequest(Reimbursement request);
+	//public void rejectRequest(Reimbursement request, Comment comment);
 }
